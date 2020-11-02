bc030c97aefeb7d3711cb7f5db6a6590ddbee7e0
dkessler <dkessler@pillartechnology.com>
2020-11-02 07:48:34 -0600

    refactor: split out each test scenario so that each test can fail or pass on it's own.  This is easier to read when something breaks.

--- test/test_number_util.py
+++ test/test_number_util.py
@@ -4,7 +4,11 @@ import number_util
 
 class TestInput(unittest.TestCase):
 
-    def test_valid_input(self):
+    def test_when_evaluating_a_positive_number_then_return_true(self):
         self.assertTrue(number_util.is_positive_integer(1))
+
+    def test_when_evaluating_zero_then_return_false(self):
         self.assertFalse(number_util.is_positive_integer(0))
+
+    def test_when_evaluating_a_negative_number_then_return_false(self):
         self.assertFalse(number_util.is_positive_integer(-1))
