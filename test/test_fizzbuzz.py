04d1f98a49725a19b128b1ee925d10a8771b8203
dkessler <dkessler@pillartechnology.com>
2020-11-02 07:44:11 -0600

    refactor: renamed tests to surface more of the intent behind the test

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -4,17 +4,14 @@ import fizz_buzz
 
 class TestFizzbuzz(unittest.TestCase):
 
-    def test_one(self):
-        self.assertEqual(str(1), fizz_buzz.process(1))
+    def test_when_processing_a_number_not_divisible_by_three_or_five_then_the_original_number_should_be_returned(self):
+        self.assertEqual(str(2), fizz_buzz.process(27))
 
-    def test_one(self):
-        self.assertEqual(str(2), fizz_buzz.process(2))
-
-    def test_fizz(self):
+    def test_when_processing_a_number_divisible_by_three_then_fizz_should_be_returned(self):
         self.assertEqual('Fizz', fizz_buzz.process(3))
 
-    def test_buzz(self):
+    def test_when_processing_a_number_divisible_by_five_then_buzz_should_be_returned(self):
         self.assertEqual('Buzz', fizz_buzz.process(5))
 
-    def test_fizzbuzz(self):
+    def test_when_processing_a_number_divisible_by_three_and_five_then_fizz_buzz_should_be_returned(self):
         self.assertEqual('FizzBuzz', fizz_buzz.process(15))
